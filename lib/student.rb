14c8221bb7e7b28d1bc365f5888f68181bbcd41b
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-23 11:32:33 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative '../lib/user.rb'
+require 'pry'
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
+# binding.pry
